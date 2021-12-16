cc693d0fb75a72e1b6eea0c9112900223e824d8d
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2021-12-16 21:00:48 +0100

    prevent null errors in web version

--- lib/persistence.dart
+++ lib/persistence.dart
@@ -4,8 +4,12 @@ import 'package:path_provider/path_provider.dart';
 import './state.dart';
 
 Future<File> getStateFile() async {
-  final directory = await getApplicationDocumentsDirectory();
-  return File('${directory.path}/state.json');
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    return File('${directory.path}/state.json');
+  } catch (e) {
+    return null;
+  }
 }
 
 startOfToday() {
@@ -15,6 +19,10 @@ startOfToday() {
 
 saveStateChanges(SlidesModel state) async {
   final file = await getStateFile();
+  if (file == null) {
+    return;
+  }
+
   state.addListener(() async {
     final json = jsonEncode(state);
     file.writeAsString(json);
@@ -22,17 +30,21 @@ saveStateChanges(SlidesModel state) async {
 }
 
 Future<SlidesModel> loadSavedState() async {
+  final emptyModel = SlidesModel();
   try {
     final file = await getStateFile();
+    if (file == null) {
+      return emptyModel;
+    }
     final json = await file.readAsString();
     final state = SlidesModel.fromJson(jsonDecode(json));
 
     if (state.date.isBefore(startOfToday())) {
       // discard saved state if it's for a past date
-      return SlidesModel();
+      return emptyModel;
     }
     return state;
   } catch (e) {
-    return SlidesModel();
+    return emptyModel;
   }
 }
